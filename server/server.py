979dcdf2c68f0ad43c7a8baeb3f60b9d702d7133
Lunesa <gmlunesa@gmail.com>
2018-03-06 23:41:31 +0800

    Added more server functions

--- server/server.py
+++ server/server.py
@@ -2,15 +2,36 @@ import zmq
 
 class Server(object):
 	def __init__(self, chat_interface, chat_port):
-        self.chat_interface = chat_interface
-        self.chat_port = chat_port
-        self.context = zmq.Context()
-        self.chat_socket = None
+		self.context = zmq.Context()
 
-    def bind_tcp_ports(self):
-    	# Reply Module
-        self.chat_socket = self.context.socket(zmq.REP)
+		self.chat_interface = chat_interface
+		self.chat_port = chat_port
+		self.chat_socket = None
 
-        chat_socket_bind_string = 'tcp://{}:{}'.format(
-            self.chat_interface, self.chat_port)
-        self.chat_socket.bind(chat_socket_bind_string)
+
+	# Bind respective TCP ports to corresponding sockets
+	def bind_tcp_ports(self):
+
+		# Reply Module
+		self.chat_socket = self.context.socket(zmq.REP)
+
+		chat_socket_bind_string = 'tcp://{}:{}'.format(self.chat_interface, self.chat_port)
+		self.chat_socket.bind(chat_socket_bind_string)
+
+
+	# Return list based on received zmq message
+	def get_message(self):
+		msg = self.chat_socket.recv_json()
+		username = msg['username']
+		message = msg['message']
+
+		return [username, message]
+
+	# Constant loop to receive messages
+	def constant_loop(self):
+		self.bind_tcp_ports()
+
+		while True:
+			username, message = self.get_message()
+
+
